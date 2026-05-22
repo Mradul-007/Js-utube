06099fcb368d6f0c0c0fa94d44951bb8920af75e
Mradul-007 <mradulshrivas007@gmail.com>
2026-05-22 12:03:59 +0000

    conversion in js

---
+++ .devcontainer/Basics/conversion.js
@@ -0,0 +1,17 @@
+//let score = "45aa"
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33"=> 33
+//"33abc"=> NaN
+//true => 1;
+
+let isLoggedIn = 1
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn);

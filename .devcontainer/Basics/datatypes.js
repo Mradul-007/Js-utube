45dcd65a0c436fa6f01236f0b46ff093b07a564a
Mradul-007 <mradulshrivas007@gmail.com>
2026-05-22 10:58:05 +0000

    Datatypes in js

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello") we are using node js, not browser
+
+let name = "mradul"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//Datatypes (primitive)
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof"Mradul");
+console.log(typeof 18);
+console.log(typeof null) // output => object
+console.log(typeof undefined)// output => undefined

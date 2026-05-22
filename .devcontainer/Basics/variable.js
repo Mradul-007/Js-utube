d9a5674e0866e03b9da051ee6a8cab3b03bb7b65
Mradul-007 <mradulshrivas007@gmail.com>
2026-05-22 10:28:53 +0000

    variable in js

---
+++ .devcontainer/Basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "mradul@gmail.com"
+var accountPassword = "12345"
+accountCity = "indore"
+let accountState
+
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+ /*
+Not to use var 
+because of issue in block scope and functional scope
+ */
